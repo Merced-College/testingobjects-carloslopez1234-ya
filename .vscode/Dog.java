a754d41bd2e0920f040dd6b0d6af38bc30b37960
carloslopez1234-ya <1234.carloslopezzzz@gmail.com>
2026-01-30 00:41:29 +0000

    commit the new code and 5 question answers.

---
+++ .vscode/Dog.java
@@ -0,0 +1,7 @@
+//carlos lopez
+// 1/27/26
+
+public class Dog {
+    // Intentionally empty for now: no fields, no constructors, no methods.
+    
+}
---
+++ .vscode/DogTest.java
@@ -0,0 +1,12 @@
+// carlos lopez
+
+public class DogTest {
+    public static void main(String[] args) {
+    // Intentionally empty for now: no fields, no constructors, no methods.
+}
+// A1: Create a Dog with no constructors defined.
+Dog dog1 = new Dog();
+
+// A2: Print the object directly.
+System.out.println("Printing dog1 directly:");
+System.out.println(dog1)
